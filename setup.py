cbdf83417c7c93286a83217c83e9c26ce1b5b845
David Kleiven <david.kleiven@ntnu.no>
2017-12-14 10:35:08 +0100

    Small changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="CEMonteCarlo",
+    version=1.0,
+    author="David Kleiven",
+    atuhor_email="davidkleiven446@gmail.com",
+    description="Monte Carlo routines for Cluster Expansion",
+    packages=["wanglandau","mcmc"]
+)
