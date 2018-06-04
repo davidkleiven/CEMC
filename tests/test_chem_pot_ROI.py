1b6f39645fea5cec39bd3a5cacfe5462da678dbc
David Kleiven <david.kleiven@ntnu.no>
2018-06-04 09:02:55 +0200

    Small change

--- tests/test_chem_pot_ROI.py
+++ tests/test_chem_pot_ROI.py
@@ -1,11 +1,12 @@
 import unittest
 skip_msg = ""
 try:
-    from .helper_functions import get_small_BC_with_ce_calc
-    from cemc.mcmc import ChemicalPotentialROI
+    from helper_functions import get_small_BC_with_ce_calc
+    from cemc.tools import ChemicalPotentialROI
     available = True
 except Exception as exc:
     available = False
+    print (str(exc))
     skip_msg = "Not available because "+str(exc)
 
 
@@ -14,12 +15,12 @@ class TestChemPotROI(unittest.TestCase):
         if not available:
             self.skipTest(skip_msg)
             return
-        no_throw = False
+        no_throw = True
         msg = ""
 
         try:
             bc = get_small_BC_with_ce_calc()
-            roi_finder = ChemicalPotentialROI(bc.atoms, symbols=["Al","Mg"])
+            roi = ChemicalPotentialROI(bc.atoms, symbols=["Al","Mg"])
             chem_pots = roi.chemical_potential_roi()
             sampling, names = roi.suggest_mu(mu_roi=chem_pots,N=10)
         except Exception as exc:
