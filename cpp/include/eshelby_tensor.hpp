32d60e7b7f6391dec7f01aabedaa3f75443ff87c
David Kleiven <david.kleiven@ntnu.no>
2018-07-11 15:24:11 +0200

    Eshelby tensor compiles

--- cpp/include/eshelby_tensor.hpp
+++ cpp/include/eshelby_tensor.hpp
@@ -1,6 +1,8 @@
 #ifndef ESHELBY_TENSOR_H
 #define ESHELBY_TENSOR_H
 #include <array>
+#include <map>
+#include <string>
 
 class EshelbyTensor
 {
@@ -9,7 +11,7 @@ public:
   virtual ~EshelbyTensor(){};
 
   /** Evaluate the Eshelby Tensor (Not implemented!)*/
-  virtual double operator()(int i, int j, int k, int l) const{return 0.0;};
+  virtual double operator()(int i, int j, int k, int l);
 
   /** Initialize elliptic integrals etc. Has to be called prior to evaluation */
   virtual void init();
@@ -21,13 +23,30 @@ protected:
   double elliptic_f;
   double elliptic_e;
   std::array<double,6> I;
+  bool require_rebuild{true};
 
   void I_tensor(std::array<double,6> &I) const;
   double evlauate_principal(int i, int j, int k, int l) const;
+  double tensor[81]; // Rank 4 Eshelby tensor
+
+  /** Return the corresponding index to the array */
+  static int get_array_indx(int i, int j, int k, int l);
+
+  /** Shift array circular */
+  static void circular_shift(double data[], int size);
 
   /** The tensor has some symmetries so we only compute
   certain combinations */
-  static void sort_indices( int indies[4]);
+  static void sort_indices(int indies[4]);
+
+  /** Convert dictionary key to arrya of indices */
+  static void key_to_array(const std::string &key, int array[4]);
+
+  /** Constructs the full Eshelby tensor */
+  void construct_full_tensor();
+
+  /** Construct elements that can use the same permuation of the semi axes */
+  void construct_ref_tensor( std::map<std::string, double> &elm, double semi_axes[3]);
 
   /** Elliptic integrals */
   virtual double F(double theta, double kappa);
--- cpp/src/eshelby_tensor.cpp
+++ cpp/src/eshelby_tensor.cpp
@@ -2,6 +2,8 @@
 #include <Python.h>
 #include <cmath>
 #include <stdexcept>
+#include <map>
+#include <string>
 
 using namespace std;
 
@@ -143,4 +145,122 @@ void EshelbyTensor::sort_indices( int orig[4] )
     orig[2] = orig[3];
     orig[3] = temp_orig2;
   }
+}
+
+double EshelbyTensor::operator()(int i, int j, int k, int l)
+{
+  if (require_rebuild)
+  {
+    construct_full_tensor();
+  }
+  // Sort the indices to take the symmetries of the tensor into account
+  int indx_in_array = get_array_indx(i, j, k, l);
+  return tensor[indx_in_array];
+}
+
+void EshelbyTensor::construct_full_tensor()
+{
+  double semi_axes[3] = {a, b, c};
+  for (unsigned int i=0;i<81;i++)
+  {
+    tensor[i] = 0.0;
+  }
+
+  for (unsigned int i=0;i<3;i++ )
+  {
+    map<string, double> ref_tensor;
+    construct_ref_tensor(ref_tensor, semi_axes);
+
+    for (auto iter=ref_tensor.begin(); iter != ref_tensor.end(); ++iter)
+    {
+      int indices[4];
+      key_to_array(iter->first, indices);
+
+      // Update the array to the current cyclic permutation
+      for (unsigned int n=0;n<4;n++)
+      {
+        indices[n] = (indices[n]+1)%3;
+      }
+
+      int indx_in_array = get_array_indx(indices[0], indices[1], indices[2], indices[3]);
+      tensor[indx_in_array] = iter->second;
+    }
+    circular_shift(semi_axes, 3);
+  }
+  require_rebuild = false;
+}
+
+void EshelbyTensor::construct_ref_tensor(map<string, double> &elements, double semi_axes[3])
+{
+  double a_cyc = semi_axes[0];
+  double b_cyc = semi_axes[1];
+  double c_cyc = semi_axes[2];
+
+  double theta = asin(sqrt(a_cyc*a_cyc - c_cyc*c_cyc)/a_cyc*a_cyc);
+  double kappa;
+  if (abs(a_cyc*a_cyc - c_cyc*c_cyc) < 1E-6)
+  {
+    kappa = 0.0;
+  }
+  else
+  {
+    kappa = sqrt((a_cyc*a_cyc - b_cyc*b_cyc) / (a_cyc*a_cyc - c_cyc*c_cyc));
+  }
+
+  double f_int = F(theta, kappa);
+  double e_int = E(theta, kappa);
+
+  double I1 = 4.0*PI*a_cyc*b_cyc*c_cyc / sqrt((a_cyc*a_cyc - b_cyc*b_cyc) * (a_cyc*a_cyc - c_cyc*c_cyc));
+  I1 *= (f_int - e_int);
+
+  double I3 = 4.0*PI*a_cyc*b_cyc*c_cyc / sqrt((b_cyc*b_cyc - c_cyc*c_cyc) * (a_cyc*a_cyc - b_cyc*b_cyc));
+  I3 *= ( (b_cyc*sqrt(a_cyc*a_cyc - c_cyc*c_cyc)) / (a_cyc*c_cyc) - e_int);
+
+  double I2 = 4.0*PI - I1 - I3;
+
+  double I12 = (I2 - I1) / (a_cyc*a_cyc - b_cyc*b_cyc);
+  double I11 = 4.0*PI/(a_cyc*a_cyc) - 3*I1/(c_cyc*c_cyc) + (b_cyc*b_cyc/(c_cyc*c_cyc) - 1.0)*I2;
+  I11 /= (3.0*(1.0 - a_cyc*a_cyc/(c_cyc*c_cyc)));
+  double I13 = 3*I1/(c_cyc*c_cyc) - 3*a_cyc*a_cyc*I11/(c_cyc*c_cyc) - b_cyc*b_cyc*I12/(c_cyc*c_cyc);
+
+  double pref = 1.0/(8.0*PI*(1-poisson));
+
+  // S_1111
+  elements["0000"] = pref*(3.0*a_cyc*a_cyc*I11 + (1.0-2*poisson)*I1);
+
+  // S_1122
+  elements["0011"] = pref*(b_cyc*b_cyc*I12 + (1.0-2.0*poisson)*I1);
+
+  // S_1133
+  elements["0022"] = pref*(c_cyc*c_cyc*I13 + (1.0-2.0*poisson)*I1);
+
+  // S_1212
+  elements["0101"] = 0.5*pref*( (a_cyc*a_cyc + b_cyc*b_cyc)*I12 - (1.0-2.0*poisson)*(I1+I2));
+
+  elements["0001"] = 0.0;
+  elements["0112"] = 0.0;
+  elements["0121"] = 0.0;
+}
+
+int EshelbyTensor::get_array_indx(int i, int j, int k, int l)
+{
+  return i*27 + j*9 + k*3 + l;
+}
+
+void EshelbyTensor::key_to_array(const string &key, int array[4])
+{
+  for (unsigned int i=0;i<4;i++)
+  {
+    array[i] = key[i] - '0';
+  }
+}
+
+void EshelbyTensor::circular_shift(double data[], int size)
+{
+  double first = data[0];
+  for (unsigned int i=0;i<size-1;i++)
+  {
+    data[i] = data[i+1];
+  }
+  data[size-1] = first;
 }
