4703805ee5ae90b28e5b0687a9808db603327b90
David Kleiven <david.kleiven@ntnu.no>
2018-08-07 09:37:58 +0200

    Autopep8

--- cemc/tools/canonical_free_energy.py
+++ cemc/tools/canonical_free_energy.py
@@ -4,18 +4,20 @@ from scipy.integrate import cumtrapz
 from scipy.interpolate import UnivariateSpline
 from scipy.optimize import minimize
 
-class CanonicalFreeEnergy( object ):
+
+class CanonicalFreeEnergy(object):
     """
     Compute the Free Enery in the Canonical Ensemble (fixed composition)
     by thermodynamic integration
 
     :param composition: Dictionary with compositions
     """
-    def __init__( self, composition, limit="hte" ):
+
+    def __init__(self, composition, limit="hte"):
         self.comp = composition
         self.limit = limit
 
-    def reference_energy( self, T, energy ):
+    def reference_energy(self, T, energy):
         """
         Computes the reference energy
 
@@ -24,10 +26,10 @@ class CanonicalFreeEnergy( object ):
         """
         if self.limit == "hte":
             infinite_temp_value = -self.inf_temperature_entropy()
-            beta = 1.0/(kB*T)
-            ref_energy = infinite_temp_value + energy*beta
+            beta = 1.0 / (kB * T)
+            ref_energy = infinite_temp_value + energy * beta
         else:
-            beta = 1.0/(kB*T)
+            beta = 1.0 / (kB * T)
             ref_energy = beta * energy
         return ref_energy
 
@@ -35,11 +37,11 @@ class CanonicalFreeEnergy( object ):
         """
         Return the entropy at infinite temperature
         """
-        concs = np.array( [value for key,value in self.comp.items()] )
-        infinite_temp_value = np.sum( concs*np.log(concs) )
+        concs = np.array([value for key, value in self.comp.items()])
+        infinite_temp_value = np.sum(concs * np.log(concs))
         return -infinite_temp_value
 
-    def sort( self, temperature, internal_energy ):
+    def sort(self, temperature, internal_energy):
         """
         Sort the value such that the largerst temperature goes first
 
@@ -54,23 +56,24 @@ class CanonicalFreeEnergy( object ):
         energy_srt = [internal_energy[indx] for indx in srt_indx]
 
         if self.limit == "hte":
-            assert( temp_srt[0] > temp_srt[1] ) # Make sure the sorting is correct
+            # Make sure the sorting is correct
+            assert(temp_srt[0] > temp_srt[1])
         else:
-            assert( temp_srt[0] < temp_srt[1] )
-        return np.array(temp_srt), np.array( energy_srt)
+            assert(temp_srt[0] < temp_srt[1])
+        return np.array(temp_srt), np.array(energy_srt)
 
-    def get( self, temperature, internal_energy ):
+    def get(self, temperature, internal_energy):
         """
         Compute the Helholtz Free Energy
 
         :param temperature: Temperature in kelvin
         :param internal_energy: Internal energy (per atom)
         """
-        temperature, internal_energy = self.sort( temperature, internal_energy )
-        betas = 1.0/(kB*temperature)
-        ref_energy = self.reference_energy( temperature[0], internal_energy[0] )
+        temperature, internal_energy = self.sort(temperature, internal_energy)
+        betas = 1.0 / (kB * temperature)
+        ref_energy = self.reference_energy(temperature[0], internal_energy[0])
         free_energy = np.zeros(len(temperature))
-        free_energy = cumtrapz(internal_energy,x=betas,initial=0.0)
+        free_energy = cumtrapz(internal_energy, x=betas, initial=0.0)
         free_energy += ref_energy
-        free_energy *= kB*temperature
+        free_energy *= kB * temperature
         return temperature, internal_energy, free_energy
