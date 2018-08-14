76e2cc7c2e252e670b4f583d702fe8f56c317aa1
Jin Chang <jchang@dtu.dk>
2018-08-14 23:22:04 +0200

    python syntax style changes.

--- cemc/mcmc/canonical_nucleation_mc.py
+++ cemc/mcmc/canonical_nucleation_mc.py
@@ -5,33 +5,36 @@ from cemc.mcmc import NetworkObserver
 import numpy as np
 from ase.visualize import view
 
-class CanonicalNucleationMC( Montecarlo ):
-    def __init__( self, atoms, T, **kwargs ):
+
+class CanonicalNucleationMC(Montecarlo):
+    def __init__(self, atoms, T, **kwargs):
         self.nuc_sampler = kwargs.pop("nucleation_sampler")
         self.network_name = kwargs.pop("network_name")
         self.network_element = kwargs.pop("network_element")
         conc = kwargs.pop("concentration")
-        if ( "mpicomm" in kwargs.keys() ):
-            kwargs.pop("mpicomm") # Remove MPI communicator if present
+        if "mpicomm" in kwargs.keys():
+            kwargs.pop("mpicomm")   # Remove MPI communicator if present
 
-        super(CanonicalNucleationMC,self).__init__( atoms, T, **kwargs )
-        self.n_atoms = {key:int(value*len(self.atoms)) for key,value in conc.items()}
-        self.network = NetworkObserver( calc=self.atoms._calc, cluster_name=self.network_name, element=self.network_element )
-        self.attach( self.network )
+        super(CanonicalNucleationMC, self).__init__(atoms, T, **kwargs)
+        self.n_atoms = {key: int(value*len(self.atoms)) for key, value
+                        in conc.items()}
+        self.network = NetworkObserver(calc=self.atoms._calc,
+                                       cluster_name=self.network_name,
+                                       element=self.network_element)
+        self.attach(self.network)
 
-    def _accept( self, system_changes ):
-        move_accepted = Montecarlo._accept( self, system_changes )
+    def _accept(self, system_changes):
+        move_accepted = Montecarlo._accept(self, system_changes)
         in_window = self.nuc_sampler.is_in_window(self.network)
         return move_accepted and in_window
 
     def _get_trial_move(self):
-        """
-        Perform a trial move
-        """
-        if ( not self.nuc_sampler.is_in_window(self.network) ):
+        """Perform a trial move."""
+        if not self.nuc_sampler.is_in_window(self.network):
             self.network(None)
-            msg = "System is outside the window before the trial move is performed!\n"
-            raise RuntimeError( msg )
+            msg = "System is outside the window before the trial move "
+            msg += "is performed!\n"
+            raise RuntimeError(msg)
             #stat = self.network.get_statistics()
             #num_clusters = stat["number_of_clusters"]
             #max_size = stat["max_size"]
@@ -45,7 +48,7 @@ class CanonicalNucleationMC( Montecarlo ):
         """
         count = {}
         for atom in self.atoms:
-            if ( atom.symbol not in count.keys() ):
+            if atom.symbol not in count.keys():
                 count[atom.symbol] = 1
             else:
                 count[atom.symbol] += 1
@@ -55,72 +58,81 @@ class CanonicalNucleationMC( Montecarlo ):
         """
         Sort the number of atoms dictionary such that the lowest enters first
         """
-        tuple_natoms = [(value,key) for key,value in self.n_atoms.items()]
+        tuple_natoms = [(value, key) for key, value in self.n_atoms.items()]
         tuple_natoms.sort()
         return tuple_natoms
 
     def add_additional_atoms_to_match_concentration(self):
         """
-        After there might be too few solute atoms after the initial cluster have been generated
+        After there might be too few solute atoms after the initial cluster
+        have been generated
         """
         atoms_count = self.get_atoms_count()
         sorted_natoms = self.sort_natoms()
         major_element = sorted_natoms[-1][1]
-        max_trial_attemps = 100*len(self.atoms)
+        max_trial_attemps = 100 * len(self.atoms)
         self.atoms._calc.clear_history()
-        for i in range(0,len(sorted_natoms)-1):
+        for i in range(0, len(sorted_natoms) - 1):
             symb = sorted_natoms[i][1]
             at_count = atoms_count[symb]
             diff = self.n_atoms[symb] - atoms_count[symb]
-            if ( diff < 0 ):
-                msg = "The system contains two many solute atoms. That should not be possible!\n"
-                msg += "Failing for atom {}. Number present: {}. Concentration requires: {}".format(symb,at_count,self.n_atoms[symb])
-                raise RuntimeError( msg )
+            if diff < 0:
+                msg = "The system contains two many solute atoms. "
+                msg += "That should not be possible!\n"
+                msg += "Failing for atom {}. ".format(symb)
+                msg += "Number present: {}. ".format(at_count)
+                msg += "Concentration requires: {}".format(self.n_atoms[symb])
+                raise RuntimeError(msg)
 
             num_inserted = 0
             num_trials = 0
-            while( num_inserted < diff and num_trials < max_trial_attemps):
-                indx = np.random.randint( low=0,high=len(self.atoms) )
+            while(num_inserted < diff and num_trials < max_trial_attemps):
+                indx = np.random.randint(low=0, high=len(self.atoms))
                 num_trials += 1
-                if ( self.atoms[indx].symbol == major_element ):
-                    syst_change = (indx,self.atoms[indx].symbol,symb)
-                    self.atoms._calc.update_cf( syst_change )
-                    if ( self.nuc_sampler.is_in_window(self.network) ):
+                if self.atoms[indx].symbol == major_element:
+                    syst_change = (indx, self.atoms[indx].symbol, symb)
+                    self.atoms._calc.update_cf(syst_change)
+                    if self.nuc_sampler.is_in_window(self.network):
                         num_inserted += 1
                         self.atoms._calc.clear_history()
                     else:
                         self.atoms._calc.undo_changes()
-            if ( num_trials >= max_trial_attemps ):
-                raise RuntimeError( "Did not manage to create a system with the correct composition!" )
+            if num_trials >= max_trial_attemps:
+                msg = "Did not manage to create a system with the correct "
+                msg += "composition!"
+                raise RuntimeError(msg)
 
         # Make sure that the system has the correct composition
         count = self.get_atoms_count()
         for key in count.keys():
-            if ( count[key] != self.n_atoms[key] ):
+            if count[key] != self.n_atoms[key]:
                 msg = "The system appears to have wrong composition.\n"
-                msg += "Current atoms count: {}. Required atoms count: {}".format(count,self.n_atoms)
-                raise RuntimeError( msg )
+                msg += "Current atoms count: {}. ".format(count)
+                msg += "Required atoms count: {}".format(self.n_atoms)
+                raise RuntimeError(msg)
 
         # Re-initialize the tracker
         self._build_atoms_list()
         self.atoms._calc.clear_history()
 
-    def run( self, nsteps=10000 ):
+    def run(self, nsteps=10000):
         """
         Run samples in each window until a desired precission is found
         """
-        if ( self.nuc_sampler.nucleation_mpicomm is not None ):
+        if self.nuc_sampler.nucleation_mpicomm is not None:
             self.nuc_sampler.nucleation_mpicomm.barrier()
 
         n_solute_atoms = self.n_atoms[self.network_element]
 
         for i in range(self.nuc_sampler.n_windows):
-            self.log( "Window {} of {}".format(i,self.nuc_sampler.n_windows) )
+            self.log("Window {} of {}".format(i, self.nuc_sampler.n_windows))
             self.nuc_sampler.current_window = i
 
-            lower,upper = self.nuc_sampler._get_window_boundaries(i)
-            if ( 0.5*(lower+upper) >= n_solute_atoms ):
-                self.log( "The concentration of solute atoms is too low to simulate this size. Aborting.")
+            lower, upper = self.nuc_sampler._get_window_boundaries(i)
+            if 0.5*(lower+upper) >= n_solute_atoms:
+                msg = "The concentration of solute atoms is too low to "
+                msg += "simulate this size. \n Aborting."
+                self.log(msg)
                 break
             self.reset()
             self.nuc_sampler.bring_system_into_window(self.network)
@@ -132,11 +144,11 @@ class CanonicalNucleationMC( Montecarlo ):
             self.nuc_sampler.mode = Mode.sample_in_window
 
             current_step = 0
-            while( current_step < nsteps ):
+            while current_step < nsteps:
                 current_step += 1
                 self._mc_step()
                 self.nuc_sampler.update_histogram(self)
                 self.network.reset()
 
-        if ( self.nuc_sampler.nucleation_mpicomm is not None ):
+        if self.nuc_sampler.nucleation_mpicomm is not None:
             self.nuc_sampler.nucleation_mpicomm.barrier()
--- cemc/mcmc/dos_mu_temp.py
+++ cemc/mcmc/dos_mu_temp.py
@@ -2,42 +2,46 @@ from cemc.mcmc import SGCCompositionFreeEnergy
 import h5py as h5
 from ase.visualize import view
 
+
 class FreeEnergyMuTempArray(object):
-    def __init__( self, temps=[], mu=[], dos_params={}, fname="free_energy.h5" ):
+    def __init__(self, temps=[], mu=[], dos_params={}, fname="free_energy.h5"):
         self.temps = temps
         self.mu = mu
         self.dos_params = {}
         self.fname = fname
         self.current_dset = 0
 
-    def get_dset_name( self, temp, pot ):
-        """
-        Generate dataset name
-        """
+    def get_dset_name(self, temp, pot):
+        """Generate dataset name."""
         mu_part = ""
-        for key,value in self.mu[0].items():
-            mu_part += "{}{}".format(key,int(value*10000))
-        dset_name = "{}K{}".format(int(temp),mu_part)
+        for key, value in self.mu[0].items():
+            mu_part += "{}{}".format(key, int(value * 10000))
+        dset_name = "{}K{}".format(int(temp), mu_part)
         return dset_name
 
-    def save_dataset( self, limits, hist, T, chem_pot ):
+    def save_dataset(self, limits, hist, T, chem_pot):
         dsetname = "sim{}".format(self.current_dset)
         self.current_dset += 1
-        with h5.File( self.fname, 'a' ) as hf:
-            dset = hf.create_dataset( dsetname, data=hist )
+        with h5.File(self.fname, 'a') as hf:
+            dset = hf.create_dataset(dsetname, data=hist)
             dset.attrs["limits"] = limits
             dset.attrs["temperature"] = T
-            for key,value in chem_pot.items():
+            for key, value in chem_pot.items():
                 dset.attrs[key] = value
 
-    def run( self, sgc_mc_obj, sgc_comp_free_eng, min_num_steps=1000 ):
+    def run(self, sgc_mc_obj, sgc_comp_free_eng, min_num_steps=1000):
         for T in self.temps:
             for chem_pot in self.mu:
-                print ("Current temperature {}K. Current chemical potential: {}".format(T,chem_pot))
+                msg = "Current temperature {}K. ".format(T)
+                msg += "Current chemical potential: {}".format(chem_pot)
+                print(msg)
                 sgc_mc_obj.reset()
                 sgc_comp_free_eng.reset()
                 sgc_mc_obj.T = T
                 sgc_mc_obj.chemical_potential = chem_pot
-                lim, hist = sgc_comp_free_eng.find_dos( sgc_mc_obj, max_rel_unc=2.0, min_num_steps=min_num_steps, rettype="raw_hist" )
-                self.save_dataset( lim, hist, T, chem_pot )
-        print ("All results stored in {}".format(self.fname))
+                lim, hist = \
+                    sgc_comp_free_eng.find_dos(sgc_mc_obj, max_rel_unc=2.0,
+                                               min_num_steps=min_num_steps,
+                                               rettype="raw_hist")
+                self.save_dataset(lim, hist, T, chem_pot)
+        print("All results stored in {}".format(self.fname))
