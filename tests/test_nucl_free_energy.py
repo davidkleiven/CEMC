1d72b720711fb4999927c2ddb972d831083e0614
David Kleiven <david.kleiven@ntnu.no>
2018-09-05 12:05:28 +0200

    Add test for checking inertial tensor of a sphere

--- tests/test_nucl_free_energy.py
+++ tests/test_nucl_free_energy.py
@@ -1,19 +1,20 @@
 import unittest
 import os
 try:
-    from cemc.mcmc import NucleationSampler,SGCNucleation, CanonicalNucleationMC, FixedNucleusMC
+    from cemc.mcmc import NucleationSampler, SGCNucleation
+    from cemc.mcmc import CanonicalNucleationMC, FixedNucleusMC
     from ase.ce import BulkCrystal
     from ase.ce import CorrFunction
     from cemc.mcmc import InertiaCrdInitializer
     from cemc import CE
     from cemc import get_ce_calc
-    from helper_functions import flatten_cluster_names
+    import numpy as np
     from helper_functions import get_ternary_BC, get_example_ecis
-    import os
     available = True
 except Exception as exc:
-    print (str(exc))
+    print(str(exc))
     available = False
+
 
 def get_network_name(cnames):
     for name in cnames:
@@ -24,8 +25,8 @@ def get_network_name(cnames):
 
 class TestNuclFreeEnergy( unittest.TestCase ):
     def test_no_throw(self):
-        if ( not available ):
-            self.skipTest( "ASE version does not have CE!" )
+        if not available:
+            self.skipTest("ASE version does not have CE!")
             return
         no_throw = True
         msg = ""
@@ -38,9 +39,9 @@ class TestNuclFreeEnergy( unittest.TestCase ):
             if os.path.exists(db_name):
                 os.remove(db_name)
             kwargs = {
-                "crystalstructure":"fcc", "a": 4.05,
-                "size":[3, 3, 3],
-                "basis_elements":[["Al", "Mg"]],
+                "crystalstructure": "fcc", "a": 4.05,
+                "size": [3, 3, 3],
+                "basis_elements": [["Al", "Mg"]],
                 "conc_args": conc_args, "db_name": db_name,
                 "max_cluster_size": 3
             }
@@ -90,29 +91,119 @@ class TestNuclFreeEnergy( unittest.TestCase ):
             no_throw = False
         self.assertTrue(no_throw, msg=msg)
 
+    def _spherical_nano_particle_matches(self, conc_init):
+        """Check the inertial tensor of a fixed nano particle is conserved."""
+        # Test some inertia calculation
+        from ase.cluster.cubic import FaceCenteredCubic
+        from random import choice
+        from ase.build import bulk
+        from itertools import product
+        from ase.ce.tools import wrap_and_sort_by_position
+
+        # Construct a spherical nano particle
+        surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
+        layers = [6, 9, 5]
+        lc = 4.05
+        nanoparticle = FaceCenteredCubic('Al', surfaces, layers,
+                                         latticeconstant=lc)
+        symbs = ["Mg", "Si"]
+        for atom in nanoparticle:
+            atom.symbol = choice(symbs)
+        pos = nanoparticle.get_positions()
+        com = np.sum(pos, axis=0) / pos.shape[0]
+        nanoparticle.translate(-com)
+        pos = nanoparticle.get_positions()
+        orig_inertia = np.zeros((3, 3))
+        for comb in product(list(range(3)), repeat=2):
+            i1 = comb[0]
+            i2 = comb[1]
+            orig_inertia[i1, i2] = np.sum(pos[:, i1] * pos[:, i2])
+
+        if not np.allclose(orig_inertia, orig_inertia.T):
+            return False, "Intertia tensor of nano particle is not symmetric!"
+        orig_principal = np.linalg.eigvalsh(orig_inertia)
+
+        # Now find the atom in the nanoparticle closest to the origin
+        # and put it exactly at the origin
+        lengths = np.sum(pos**2, axis=1)
+        indx = np.argmin(lengths)
+        nanoparticle.translate(-nanoparticle[indx].position)
+        blk = bulk("Al", crystalstructure="fcc", a=lc)
+
+        blk = blk * (20, 20, 20)
+        blk = wrap_and_sort_by_position(blk)
+        cell = blk.get_cell()
+        diag = 0.5 * (cell[0, :] + cell[1, :] + cell[2, :])
+        nanoparticle.translate(diag)
+        pos = nanoparticle.get_positions()
+
+        # Inser the nano particle
+        pos_blk = blk.get_positions()
+        used_sites = []
+
+        for i, atom in enumerate(nanoparticle):
+            diff = pos_blk - pos[i, :]
+            lengths = np.sqrt(np.sum(diff**2, axis=1))
+            indx = np.argmin(lengths)
+            if indx in used_sites:
+                return False, "Two symbols have the same closest atom!"
+            blk[indx].symbol = atom.symbol
+            used_sites.append(indx)
+
+        # Apply some translation
+        trans = 1.2 * diag
+        blk.translate(trans)
+        blk = wrap_and_sort_by_position(blk)
+        # Manually alter the atoms object and rebuild the atoms list
+        conc_init.fixed_nucl_mc.atoms = blk
+        conc_init.fixed_nucl_mc._build_atoms_list()
+
+        # Inertia tensor
+        inertia_tens = conc_init.principal_inertia
+        if not np.allclose(inertia_tens, orig_principal, rtol=1E-4):
+            msg = "Inertia tensor does not match\n"
+            msg += "Original: {}\n".format(orig_principal)
+            msg += "Calculated from bulk: {}\n".format(inertia_tens)
+            return False, msg
+        return True, ""
+
+
     def test_with_inertia_reac_crd(self):
         if not available:
             self.skipTest("ASE version does not have CE!")
 
-        bc = get_ternary_BC()
-        ecis = get_example_ecis(bc=bc)
-        calc = CE(bc, eci=ecis)
-        bc.atoms.set_calculator(calc)
+        msg = ""
+        no_throw = True
+        try:
+            bc = get_ternary_BC()
+            ecis = get_example_ecis(bc=bc)
+            calc = CE(bc, eci=ecis)
+            bc.atoms.set_calculator(calc)
 
-        T = 200
-        nn_names = [name for name in bc.cluster_family_names if int(name[1]) == 2]
-        mc = FixedNucleusMC(
-            bc.atoms, T, network_name=nn_names, network_element=["Mg", "Si"])
-        conc_init = InertiaCrdInitializer(
-            fixed_nucl_mc=mc, matrix_element="Al",
-            cluster_elements=["Mg", "Si"])
-        elements = {"Mg": 4, "Si": 4}
-        mc.insert_symbol_random_places("Mg", num=4, swap_symbs=["Al"])
-        mc.insert_symbol_random_places("Si", num=4, swap_symbs=["Al"])
-        mc.runMC(steps=100, elements=elements)
+            T = 200
+            nn_names = [name for name in bc.cluster_family_names
+                        if int(name[1]) == 2]
 
-        print(conc_init.normalized_principal_inertia)
-        self.assertTrue(True)
+            mc = FixedNucleusMC(
+                bc.atoms, T, network_name=nn_names,
+                network_element=["Mg", "Si"])
+
+            conc_init = InertiaCrdInitializer(
+                fixed_nucl_mc=mc, matrix_element="Al",
+                cluster_elements=["Mg", "Si"])
+            elements = {"Mg": 4, "Si": 4}
+            mc.insert_symbol_random_places("Mg", num=4, swap_symbs=["Al"])
+            mc.insert_symbol_random_places("Si", num=4, swap_symbs=["Al"])
+            mc.runMC(steps=100, elements=elements)
+
+            match, match_msg = self._spherical_nano_particle_matches(conc_init)
+            self.assertTrue(match, msg=match_msg)
+        except Exception as exc:
+            no_throw = False
+            msg = str(exc)
+
+        self.assertTrue(no_throw, msg=msg)
+
 
 if __name__ == "__main__":
     from cemc import TimeLoggingTestRunner
