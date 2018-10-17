37047008f416cda0be4aff323bc9e17cea684297
David Kleiven <davidkleiven446@gmail.com>
2018-10-17 10:41:06 +0200

    Minor change

--- cemc/mcmc/montecarlo.py
+++ cemc/mcmc/montecarlo.py
@@ -972,7 +972,6 @@ class Montecarlo(object):
         for bias in self.bias_potentials:
             self.new_bias_energy += bias(system_changes)
         self.new_energy += self.new_bias_energy
-
         self.last_energies[1] = self.new_energy
 
         if (self.is_first):
