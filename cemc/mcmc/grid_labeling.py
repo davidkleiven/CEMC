bb2a3a6ed5e4a7301e7505f111f4d1d32d2733f2
David Kleiven <davidkleiven446@gmail.com>
2018-10-23 12:54:00 +0200

    Added some properties

--- cemc/mcmc/grid_labeling.py
+++ cemc/mcmc/grid_labeling.py
@@ -15,6 +15,7 @@ class GridLabeling(object):
         self.inv_bin_shape = np.linalg.inv(self.bin_shape)
         self.bins = np.zeros(size, dtype=np.uint8)
         self.pos = self.atoms.get_positions()
+        self.clusters = np.zeros_like(self.bins)
 
     def get_bin(self, atom_indx):
         """Return the bin given an atom index."""
@@ -28,8 +29,49 @@ class GridLabeling(object):
            The algrotihm will count how many of each element
            there is in each bin
         """
+        from cemc_cpp_code import hoshen_kopelman
         self.bins[:, :, :] = 0
         for atom in self.atoms:
             if atom.symbol in self.track_elements:
                 n = self.get_bin(atom.index)
                 self.bins[n[0], n[1], n[2]] += 1
+
+        # Run the Hoshen-Kopelman algorithm to label the 
+        # bins into clusters
+        self.clusters = hoshen_kopelman(self.bins)
+
+    @property
+    def num_clusters(self):
+        return np.max(self.clusters)
+
+    @property
+    def cluster_sizes(self):
+        return np.bincount(self.clusters)
+
+    def get_bin_changes(self, system_changes):
+        """Return the changes to the bins produced by this move."""
+        bin_changes = {}
+        for change in system_changes:
+            new_bin = tuple(self.get_bin(change[0]))
+            if change[1] not in self.track_elements and change[2] in self.track_elements:
+                bin_changes[new_bin] = bin_changes.get(new_bin, 0) + 1
+            elif change[1] in self.track_elements and change[2] not in self.track_elements:
+                bin_changes[new_bin] = bin_changes.get(new_bin, 0) - 1
+        return bin_changes
+
+    def move_require_cluster_update(self, system_changes):
+        """Return True if the move require updating the clusters."""
+        bin_changes = self.get_bin_changes(system_changes)
+        
+        for k, v in bin_changes.items():
+            if self.bins[k] == 0 and v > 0:
+                # An empty bin becomes occupied
+                return True
+            elif v < 0 and abs(v) >= self.bins[k]:
+                # An occupied bin becomes unoccupied
+                return True
+
+        # No new occupied bins are created by this move
+        # and no occupied bins become unoccupied
+        return False
+
