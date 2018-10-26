085c79aad17166fa5cce46291d6512d765ee6be6
David Kleiven <davidkleiven446@gmail.com>
2018-10-26 15:05:02 +0200

    Test script for the adaptive sampler

---
+++ tests/test_adaptive_bias_potential.py
@@ -0,0 +1,57 @@
+import unittest
+try:
+    from cemc.mcmc import ReactionCrdInitializer
+    from cemc.mcmc import Montecarlo
+    from cemc.mcmc import AdaptiveBiasReactionPathSampler
+    from cemc import CE
+    from helper_functions import get_ternary_BC
+    from helper_functions import get_example_ecis
+
+    class DummyReacCrdInit(ReactionCrdInitializer):
+        def get(self, atoms, system_changes=[]):
+            return 0.0
+
+        def set(self, value):
+            pass
+
+        def __call__(self, system_changes):
+            return 0.0
+
+    skipMsg = ""
+    available = True
+except ImportError as exc:
+    skipMsg = str(exc)
+    available = False
+
+
+class TestAdaptiveBiasReacPath(unittest.TestCase):
+    def test_no_throw(self):
+        if not available:
+            self.skipTest(skipMsg)
+
+        no_throw = True
+        msg = ""
+        try:
+            bc = get_ternary_BC()
+            eci = get_example_ecis()
+
+            calc = CE(bc, eci=eci)
+            bc.atoms.set_calculator(calc)
+
+            mc = Montecarlo(bc.atoms, 1000)
+
+            # Note: convergence_factor should never be
+            # set to a negative value! Here it is done
+            # to make the test converged after one step
+            bias = AdaptiveBiasReactionPathSampler(
+                mc_obj=mc, react_crd_init=DummyReacCrdInit(), 
+                react_crd=[-1.0, 1.0], convergence_factor=-1.0)
+            bias.run()
+        except Exception as exc:
+            msg = str(exc)
+            no_throw = False
+        self.assertTrue(no_throw, msg=msg)
+
+if __name__ == "__main__":
+    from cemc import TimeLoggingTestRunner
+    unittest.main(testRunner=TimeLoggingTestRunner)
