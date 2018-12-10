6bed27e2e58fc5dd023e3f4b2e4a415130530d67
David Kleiven <davidkleiven446@gmail.com>
2018-12-10 10:06:32 +0100

    Initialization function and call method

--- cemc/mcmc/strain_energy_bias.py
+++ cemc/mcmc/strain_energy_bias.py
@@ -1,12 +1,43 @@
 from cemc.mcmc import BiasPotential
+from cemc.mcmc import InertiaTensorObserver
+from cemc.tools import StrainEnergy
+from cemc.tools import rotate_tensor, rotate_rank4_mandel
+import numpy as np
 
 class Strain(BiasPotential):
-    def __init__(self, mc_sampler=None):
+    def __init__(self, mc_sampler=None, cluster_elements=[], C_matrix=None,
+                 C_prec=None, misfit=None):
         from cemc.mcmc import FixedNucleusMC
         if not isinstance(mc_sampler, FixedNucleusMC):
             raise TypeError("mc_sampler has to be of type FixedNuceus sampler!")
         self.mc = mc_sampler
         self._volume = 10.0
+        self.inert_obs = InertiaTensorObserver(atoms=self.mc.atoms, 
+                                               cluster_elements=cluster_elements)
+        self.mc.attach(self.inert_obs)
+        self.misfit = misfit
+        self.C_matrix = C_matrix
+        self.C_prec = C_prec
+
+    def initialize(self):
+        """Initialize the bias potential."""
+        num_solutes = self.mc.num_atoms_in_cluster
+        vol_per_atom = self.mc.atoms.volume()/len(self.mc.atoms)
+        self._volume = num_solutes*vol_per_atom
 
     def __call__(self, system_changes):
-        return 0.0
+        self.inert_obs(system_changes)
+
+        _, rot_matrix = np.linalg.eig(self.inert_obs.inertia)
+        C_mat = rotate_rank4_mandel(self.C_matrix, rot_matrix)
+        C_prec = rotate_rank4_mandel(self.C_prec, rot_matrix)
+        misfit = rotate_tensor(self.misfit, rot_matrix)
+
+        str_eng = StrainEnergy(aspect=self.inert_obs.ellipsoid_axes(), 
+                               misfit=misfit)
+        str_energy = str_eng.strain_energy(C_matrix=C_mat, C_prec=C_prec)
+
+        # Bias potential should not alter the
+        # observer
+        self.inert_obs.undo_last()
+        return str_energy*self._volume
