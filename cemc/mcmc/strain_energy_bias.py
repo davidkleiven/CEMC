01bb6b91d884bae65385b74a466626b4277dc290
David Kleiven <davidkleiven446@gmail.com>
2018-12-12 16:54:05 +0100

    Convert covariance tensor to inertia tensor

--- cemc/mcmc/strain_energy_bias.py
+++ cemc/mcmc/strain_energy_bias.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class Strain(BiasPotential):
     def __init__(self, mc_sampler=None, cluster_elements=[], C_matrix=None,
-                 C_prec=None, misfit=None):
+                 C_prec=None, misfit=None, poisson=0.3):
         from cemc.mcmc import FixedNucleusMC
         if not isinstance(mc_sampler, FixedNucleusMC):
             raise TypeError("mc_sampler has to be of type FixedNuceus sampler!")
@@ -18,6 +18,7 @@ class Strain(BiasPotential):
         self.misfit = misfit
         self.C_matrix = C_matrix
         self.C_prec = C_prec
+        self.poisson = poisson
 
     def initialize(self):
         """Initialize the bias potential."""
@@ -28,15 +29,19 @@ class Strain(BiasPotential):
     def __call__(self, system_changes):
         self.inert_obs(system_changes)
 
-        principal, rot_matrix = np.linalg.eigh(self.inert_obs.inertia)
+        inertia_tensor = np.trace(self.inert_obs.inertia)*np.eye(3) - self.inert_obs.inertia
+        principal, rot_matrix = np.linalg.eigh(inertia_tensor)
 
-        C_mat = rotate_rank4_mandel(self.C_matrix, rot_matrix)
-        C_prec = rotate_rank4_mandel(self.C_prec, rot_matrix)
-        misfit = rotate_tensor(self.misfit, rot_matrix)
+        # The rotation to be applied to the material properties
+        # and the misfit, is the transpose of the eigenvector
+        # matrix
+        C_mat = rotate_rank4_mandel(self.C_matrix, rot_matrix.T)
+        C_prec = rotate_rank4_mandel(self.C_prec, rot_matrix.T)
+        misfit = rotate_tensor(self.misfit, rot_matrix.T)
 
         axes = self.ellipsoid_axes(principal)
         str_eng = StrainEnergy(aspect=axes, 
-                               misfit=misfit)
+                               misfit=misfit, poisson=self.poisson)
         str_energy = str_eng.strain_energy(C_matrix=C_mat, C_prec=C_prec)
 
         # Bias potential should not alter the observer
