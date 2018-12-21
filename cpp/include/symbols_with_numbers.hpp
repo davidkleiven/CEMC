0717d768e6502a4177fe0a9347d461bde5a37ed8
David Kleiven <davidkleiven446@gmail.com>
2018-12-21 11:20:39 +0100

    Copy constructor and assignment operator

--- cpp/include/symbols_with_numbers.hpp
+++ cpp/include/symbols_with_numbers.hpp
@@ -3,13 +3,17 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 
 typedef std::vector<std::string> vec_str_t;
+typedef std::set<std::string> set_str_t;
 typedef std::map<std::string, unsigned int> dict_uint_t;
 class Symbols
 {
 public:
-    Symbols(const vec_str_t &symbs, const vec_str_t &unique_symbs);
+    Symbols(const vec_str_t &symbs, const set_str_t &unique_symbs);
+    Symbols(const Symbols &other);
+    Symbols& operator =(const Symbols &other);
     ~Symbols();
 
     /** Return the symbol ID of the atom at site indx */
@@ -21,14 +25,26 @@ public:
     /** Set a new symbol */
     void set_symbol(unsigned int indx, const std::string &symb);
 
+    /** Get symbol at position n*/
+    const std::string& get_symbol(unsigned int n) const {return symbols[n];};
+
     /** Get the array of symbols */
     const vec_str_t& get_symbols() const {return symbols;};
 
     /** Return the size of the symbol container */
     unsigned int size() const {return symbols.size();};
+
+    /** Re-initialize all symbols */
+    void set_symbols(const vec_str_t& new_symbs);
 private:
     unsigned int *symb_ids{nullptr};
     vec_str_t symbols;
     dict_uint_t symb_id_translation;
+
+    /** Syncronize the IDs with the symbols vector */
+    void update_ids();
+
+    /** Transfer members to other class */
+    void swap(Symbols &other) const;
 };
 #endif
