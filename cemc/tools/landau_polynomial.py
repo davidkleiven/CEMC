a5c356e6317c32a4781390438d3d7ef4cff20a53
David Kleiven <davidkleiven446@gmail.com>
2019-01-29 13:00:52 +0100

    Added comments

--- cemc/tools/landau_polynomial.py
+++ cemc/tools/landau_polynomial.py
@@ -6,6 +6,16 @@ from scipy.optimize import minimize, root, brentq
 from copy import deepcopy
 
 class TwoPhaseLandauPolynomial(object):
+    """Class for fitting a Landau polynomial to free energy data
+
+    :param float c1: Center concentration for the first phase
+    :param float c2: Center concentration for the second phase
+    :param np.ndarray init_guess: Initial guess for the parameters
+        The polynomial fitting is of the form
+        A*(x - c1)^2 + B*(x-c2)*y^2 + C*y^4 + D*y^6
+        This array should therefore contain initial guess
+        for the four parameter A, B, C and D.
+    """
     def __init__(self, c1=0.0, c2=1.0, num_dir=3, init_guess=None):
         self.coeff = np.zeros(4)
         self.c1 = c1
@@ -13,9 +23,12 @@ class TwoPhaseLandauPolynomial(object):
         self.init_guess = init_guess
 
     def equil_shape_order(self, conc):
+        """Calculate the equillibrium shape concentration.
+        
+        :param float conc: Concentration
+        """
         if abs(self.coeff[3] < 1E-8):
             n_eq = -0.5*self.coeff[1]*(conc - self.c2)/self.coeff[2]
-            print(conc, n_eq)
             if n_eq < 0.0:
                 return 0.0
             return n_eq
@@ -30,8 +43,11 @@ class TwoPhaseLandauPolynomial(object):
             return 0.0
         return n_eq
 
-    def eval_at_equil(self, conc, on_off_center=False):
-        """Evaluate the free energy at equillibrium order."""
+    def eval_at_equil(self, conc):
+        """Evaluate the free energy at equillibrium order.
+        
+        :param float conc: Concentration
+        """
         n_eq_sq = self.equil_shape_order(conc)
         return self.coeff[0]*(conc-self.c1)**2 + \
             self.coeff[1]*(conc - self.c2)*n_eq_sq + \
@@ -39,8 +55,13 @@ class TwoPhaseLandauPolynomial(object):
             self.coeff[3]*n_eq_sq**3
 
     def fit(self, conc1, F1, conc2, F2):
-        """Fit the free energy functional."""
-
+        """Fit the free energy functional.
+        
+        :param numpy.ndarray conc1: Concentrations in the first phase
+        :param numpy.ndarray F1: Free energy in the first phase
+        :param numpy.ndarray conc2. Concentrations in the second phase
+        :param numpy.ndarray F2: Free energy in the second phase
+        """
         conc = np.concatenate((conc1, conc2))
         free_energy = np.concatenate((F1, F2))
         S1 = np.sum(F1*(conc1 - self.c1)**2)
