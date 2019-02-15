04bd3cbd93e404fad16527fa95a32e5e1a229e45
David Kleiven <davidkleiven446@gmail.com>
2019-02-15 17:17:43 +0100

    Overload to support evaluation by node index

--- phasefield_cxx/include/tools.hpp
+++ phasefield_cxx/include/tools.hpp
@@ -1,7 +1,7 @@
 #ifndef PHASEFIELD_TOOLS_H
 #define PHASEFIELD_TOOLS_H
-#include "mmsp.grid.h"
-#include "mmsp.vector.h"
+#include "MMSP.grid.h"
+#include "MMSP.vector.h"
 
 template<int dim, typename T>
 T partial_double_derivative(const MMSP::grid<dim, T> &GRID, const MMSP::vector<int> &x, unsigned int dir){
@@ -15,5 +15,11 @@ T partial_double_derivative(const MMSP::grid<dim, T> &GRID, const MMSP::vector<i
 
     double weight = 1.0/pow(dx(GRID, dir), 2.0);
     return weight*(yh - 2.0*y + yl);
+}
+
+template<int dim, typename T>
+T partial_double_derivative(const MMSP::grid<dim, T> &GRID, unsigned int node_index, unsigned int dir){
+    vector<int> x = GRID.position(node_index);
+    return partial_double_derivative(GRID, x, dir);
 }
 #endif
