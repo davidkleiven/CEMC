df3516c813749a19c6b11278e026ba391b52ea05
David Kleiven <davidkleiven446@gmail.com>
2019-03-03 18:10:56 +0100

    PEP8

--- cemc/mcmc/adaptive_bias_reac_path.py
+++ cemc/mcmc/adaptive_bias_reac_path.py
@@ -11,19 +11,19 @@ import traceback
 class AdaptiveBiasPotential(BiasPotential):
     """Bias potential intended to be used by AdaptiveBiasReactionPathSampler
 
-    :param list lim: List of length 2 with upper and lower bound of the 
+    :param list lim: List of length 2 with upper and lower bound of the
         reaction coordinate. Ex. [0.0, 1.0]
     :param int n_bins: Number of bins used in the histogram
     :param float mod_factor: Modification factor. Each time
-        the samplers is a bin, this factor is added to the 
+        the samplers is a bin, this factor is added to the
         potential.
-    :param ReactionCrdInitializer reac_init: Initializer to 
-        which can obtain the reaction coordinate of 
+    :param ReactionCrdInitializer reac_init: Initializer to
+        which can obtain the reaction coordinate of
         an arbitrary structure
     :param float T: Temperature in Kelvin
     :param Montecarlo mc: Monte Carlo object which samples
         the configurational space
-    :param str db_bin_data: Database where one structure in each 
+    :param str db_bin_data: Database where one structure in each
         bin will be stored
     """
     def __init__(self, lim=[0.0, 1.0], n_bins=100, mod_factor=0.01,
@@ -88,7 +88,7 @@ class AdaptiveBiasPotential(BiasPotential):
         self.bias_array[bin_indx] += self.mod_factor
         new_val = self.get_bias_potential(x)
 
-        # We need to make MC aware of that the 
+        # We need to make MC aware of that the
         # energy of the current state changes
         # despite the fact that no move is performed
         self.mc.current_energy += (new_val - cur_val)
@@ -104,7 +104,7 @@ class AdaptiveBiasPotential(BiasPotential):
         from random import choice
         db = connect(self.db_bin_data)
         candidates = []
-        scond = [("bin_indx", ">", bin_range[0]), 
+        scond = [("bin_indx", ">", bin_range[0]),
                  ("bin_indx", "<", bin_range[1])]
         for row in db.select(scond):
             candidates.append(row.toatoms())
@@ -153,21 +153,22 @@ class AdaptiveBiasPotential(BiasPotential):
     def __call__(self, system_changes):
         """Return the bias potential after changes are applied.
 
-        :param system_changes: Changes to be applied. 
+        :param system_changes: Changes to be applied.
             Example: [(10, "Al", "Mg"), (20, "Mg", "Al)]
         :type system_changes: List of tuples
 
         :return: Bias potential after changes have been applied
         :rtype: float
         """
-        # We require initializers that can 
+        # We require initializers that can
         # get apply the system changes
         value = self.observer(system_changes, peak=True)[self.value_name]
         return self.get_bias_potential(value)
 
     def calculate_from_scratch(self, atoms):
         """Calculate the potential from scratch."""
-        value = self.observer.calculate_from_scratch(self.mc.atoms)[self.value_name]
+        cur_val = self.observer.calculate_from_scratch(self.mc.atoms)
+        value = cur_val[self.value_name]
         return self.get_bias_potential(value)
 
     def shift_upper_part_of_bias_array(self, indx, new_value):
@@ -186,7 +187,7 @@ class AdaptiveBiasReactionPathSampler(object):
     """Sample the free energy along a path by adaptively tuning a bias potential.
 
     :param Montecarlo mc_obj: Monte Carlo sampler
-    :param ReactionCrdInitializer reac_crd_init: Initializer that 
+    :param ReactionCrdInitializer reac_crd_init: Initializer that
         can both set and get the reaction coordinate of an arbitrary
         configuration. Benefitial if this support fast evaluation
         of the reaction coordinate if the proposed system changes
@@ -195,16 +196,16 @@ class AdaptiveBiasReactionPathSampler(object):
     :param str data_file: HDF5 file for data backup
     :param list reac_crd: List of length 2 with the upper
         and the lower value of the reaction coordinate.
-        If it is possible for the MC sampler to leave the 
-        region of interest, the MC sampler should have a 
+        If it is possible for the MC sampler to leave the
+        region of interest, the MC sampler should have a
         constraint prohibiting this attached.
-    :param float mod_factor: Modification factor used to 
+    :param float mod_factor: Modification factor used to
         update the bias potential
     :param float convergence_factor: If the bin that has been
         is least frequently visited have been visited more than
         convergence_factr*<average visits> the algorithm
         will consider the visit histogram as flat.
-    :param int save_interval: Interval between writing backup to file 
+    :param int save_interval: Interval between writing backup to file
         in seconds
     :param int log_msg_interval: Interval in seconds between every
         time a status message is printed.
@@ -212,16 +213,17 @@ class AdaptiveBiasReactionPathSampler(object):
         in a database.
     :param bool delete_db_if_exists: If True any existing DB containing
         structures will be delted prior to the run.
-    :param bool ignore_equil_steps: If True MC trial steps that leads to no 
+    :param bool ignore_equil_steps: If True MC trial steps that leads to no
         change in the reaction coordinate will not be counted
     """
-    def __init__(self, mc_obj=None, observer=None, n_bins=100, 
+    def __init__(self, mc_obj=None, observer=None, n_bins=100,
                  data_file="adaptive_bias_path_sampler.h5",
                  react_crd=[0.0, 1.0], mod_factor=0.01, convergence_factor=0.8,
-                 save_interval=600, log_msg_interval=30, db_struct="adaptive_bias.db",
-                 delete_db_if_exists=False, mpicomm=None,
-                 check_convergence_interval=10000, check_user_input=True,
-                 ignore_equil_steps=True, react_crd_name=""):
+                 save_interval=600, log_msg_interval=30,
+                 db_struct="adaptive_bias.db", delete_db_if_exists=False,
+                 mpicomm=None, check_convergence_interval=10000,
+                 check_user_input=True, ignore_equil_steps=True,
+                 react_crd_name=""):
 
         self.bias = AdaptiveBiasPotential(lim=react_crd, n_bins=n_bins,
                                           mod_factor=mod_factor,
@@ -254,7 +256,7 @@ class AdaptiveBiasReactionPathSampler(object):
         for cnst in self.mc.constraints:
             if isinstance(cnst, ReactionCrdRangeConstraint):
                 self.rng_constraint = cnst
-        
+
         self.min_window_width = 10
         self.connection = None
         self.current_min_bin = 0
@@ -285,7 +287,7 @@ class AdaptiveBiasReactionPathSampler(object):
             warning_printed = True
             self.log("Warning! Do you really want to log with as little as "
                      "{} sec interval?".format(self.output_every))
-        
+
         if self.save_interval < 60:
             warning_printed = True
             self.log("Warning! Do you really want to save backup every "
@@ -295,7 +297,6 @@ class AdaptiveBiasReactionPathSampler(object):
         if warning_printed:
             self.log("To supress these messages set check_user_input=False")
 
-    
     @property
     def support_adaptive_windows(self):
         return isinstance(self.rng_constraint, ReactionCrdRangeConstraint)
@@ -304,10 +305,12 @@ class AdaptiveBiasReactionPathSampler(object):
         """Print a summary of the current parameters."""
         self.log("Temperature: {}".format(self.mc.T))
         self.log("Modification factor: {}".format(self.bias.mod_factor))
-        self.log("Reaction coordinate: [{}, {})".format(self.bias.xmin, self.bias.xmax))
+        self.log("Reaction coordinate: [{}, {})".format(self.bias.xmin,
+                                                        self.bias.xmax))
         self.log("Save every: {} min".format(self.save_interval/60))
         self.log("Log message every: {} sec".format(self.output_every))
-        self.log("Support adaptive windows: {}".format(self.support_adaptive_windows))
+        self.log("Support adaptive windows: {}"
+                 "".format(self.support_adaptive_windows))
 
     def load_bias(self):
         """Try to load the bias potential from file."""
@@ -351,10 +354,11 @@ class AdaptiveBiasReactionPathSampler(object):
         """Make the energy curve continuous."""
         if self.connection is None:
             return
-        
+
         assert self.current_min_bin == self.connection["bin"]
 
-        self.bias.shift_upper_part_of_bias_array(self.connection["bin"], self.connection["value"])
+        self.bias.shift_upper_part_of_bias_array(self.connection["bin"],
+                                                 self.connection["value"])
         self.connection["value"] = self.bias.bias_array[self.connection["bin"]]
 
     def update_active_window(self):
@@ -366,13 +370,13 @@ class AdaptiveBiasReactionPathSampler(object):
         self.current_max_val = np.max(self.visit_histogram)
         self.current_min_val = minval
         self.average_visits = mean
-        
+
         indx = self._first_non_converged_bin()
         if indx == -1:
             self._make_energy_curve_continuous()
             self.connection = None
             return True
-        
+
         local_conv_ok = abs(indx - self.current_min_bin) > self.min_window_width
         remaining_ok = (indx < len(self.visit_histogram) - self.min_window_width - 1)
         if local_conv_ok and remaining_ok:
@@ -380,11 +384,11 @@ class AdaptiveBiasReactionPathSampler(object):
             if new_structure is None:
                 # We don't now any valid structure so we can't do anything
                 return False
-            
+
             self._make_energy_curve_continuous()
             self.current_min_bin = indx - 1
             self.bias.lowest_active_indx = self.current_min_bin
-            self.connection = {"bin": indx - 1, 
+            self.connection = {"bin": indx - 1,
                                "value": self.bias.bias_array[indx-1]}
             current_range = self.rng_constraint.range
             current_range[0] = self.bias.get_value(self.current_min_bin)
@@ -395,7 +399,8 @@ class AdaptiveBiasReactionPathSampler(object):
             self.mc.set_symbols(symbs)
 
             # Enforce a calculation of the reaction coordinate
-            value = self.bias.observer.calculate_from_scratch(self.mc.atoms)[self.bias.value_name]
+            cur_val = self.bias.observer.calculate_from_scratch(self.mc.atoms)
+            value = cur_val[self.bias.value_name]
             self.log("Window shrinked")
             self.log("New value: {}. New range: [{}, {})"
                      "".format(value, current_range[0], current_range[1]))
@@ -432,9 +437,10 @@ class AdaptiveBiasReactionPathSampler(object):
         self.log("Visits: Min: {}. Max: {}. Avg: {}. Num visited: {}. "
                  "Last visited bin: {}. Acc. ratio: {}%. Lower cut: {}"
                  "".format(self.current_min_val, self.current_max_val,
-                           self.average_visits, num_visited, self.last_visited_bin,
-                           acc_rate, self.current_min_bin))
-        self.log("Current formua: {}".format(self.mc.atoms.get_chemical_formula()))
+                           self.average_visits, num_visited,
+                           self.last_visited_bin, acc_rate,
+                           self.current_min_bin))
+        self.log("Formula: {}".format(self.mc.atoms.get_chemical_formula()))
 
     @property
     def rank(self):
@@ -479,7 +485,9 @@ class AdaptiveBiasReactionPathSampler(object):
             # We want to always write the continuous potential to file
             bias_array = self.bias.bias_array.copy()
             if self.connection is not None:
-                diff = bias_array[self.connection["bin"]] - self.connection["value"]
+                diff = bias_array[self.connection["bin"]] - \
+                        self.connection["value"]
+
                 bias_array[self.connection["bin"]:] -= diff
 
             with h5.File(self.data_file, flag) as hfile:
@@ -495,8 +503,9 @@ class AdaptiveBiasReactionPathSampler(object):
                 else:
                     hfile.create_dataset("bias", data=bias_array)
 
-                if not "x" in hfile.keys():
-                    x = np.linspace(self.bias.xmin, self.bias.xmax, len(self.visit_histogram))
+                if "x" not in hfile.keys():
+                    x = np.linspace(self.bias.xmin, self.bias.xmax,
+                                    len(self.visit_histogram))
                     hfile.create_dataset("x", data=x)
             self.log("Current state written to {}".format(self.data_file))
         self.barrier()
@@ -529,10 +538,10 @@ class AdaptiveBiasReactionPathSampler(object):
 
     def _trial_move_alter_reac_crd(self, trial_move):
         """Return True if the trial move alter the reaction coordinate."""
-
+        obs = self.bias.observer
         if self.move_accpted:
-            trial_val = self.bias.observer.get_current_value()[self.bias.value_name]
+            trial_val = obs.get_current_value()[self.bias.value_name]
         else:
-            trial_val = self.bias.observer(trial_move, peak=True)[self.bias.value_name]
+            trial_val = obs(trial_move, peak=True)[self.bias.value_name]
         tol = 1E-6
         return abs(self.current_reac_value - trial_val) > tol
