460393a765463026df7a810a9e6667e05854e147
David Kleiven <davidkleiven446@gmail.com>
2019-03-04 15:33:07 +0100

    Add doc string

--- cemc/mcmc/minimal_energy_path.py
+++ cemc/mcmc/minimal_energy_path.py
@@ -5,6 +5,23 @@ import numpy as np
 
 
 class MinimalEnergyPath(object):
+    """
+    Class for calculating the minimum energy configurations along
+    an arbitrary path
+
+    :param Montecarlo mc_obj: Monte Carlo object controling the
+        dynamics of the system
+    :param MCObserver observer: Monte Carlo observer giving the
+        used for calculating the value of the reaction coordinate
+    :param str value_name: Name of the reaction coordinate in the
+        dictionary returned by the MCObserver
+    :param int relax_steps: Number of steps used to relax
+        the system after a new configuration is found
+    :param int search_steps: Number of steps used to search
+        for a new configuration
+    :param str traj_file: Trajectory file for all structures
+    :param int max_reac_crd: Maximum value for the reaction coordinate
+    """
     def __init__(self, mc_obj=None, observer=None, value_name="",
                  relax_steps=1000, search_steps=1000,
                  traj_file="minimal_energy_path.traj",
@@ -27,6 +44,10 @@ class MinimalEnergyPath(object):
         self.max_reac_crd = max_reac_crd
 
     def relax(self):
+        """
+        Relax the system without altering the reaction
+        coordinate
+        """
         try:
             for _ in range(self.relax_steps):
                 self.mc_obj._mc_step()
@@ -37,6 +58,10 @@ class MinimalEnergyPath(object):
         self.traj.write(self.mc_obj.atoms)
 
     def find_new_config(self):
+        """
+        Find the structure that increase the value of the reaction
+        coordinate, that leads to the minimal change in energy
+        """
         # Try for the same abount of steps to find
         # a new configuration that increases the reaction
         # coordinate
@@ -74,7 +99,20 @@ class MinimalEnergyPath(object):
         print(msg)
 
     def run(self):
+        """
+        Find the minimal energy path
+        """
         while self.current_value < self.max_reac_crd:
             self.log("Current reac crd: {}".format(self.current_value))
             self.relax()
             self.find_new_config()
+
+    def save(self, fname="minimal_energy.csv"):
+        """
+        Save the final result in a CSV file
+
+        :param str fname: Filename
+        """
+        data = np.vstack([self.reac_crd, self.energy])
+        np.savetxt(fname, data, header="Reaction crd, energy (eV)")
+        self.log("Final results stored in {}".format(fname))
