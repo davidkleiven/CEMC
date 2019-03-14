3a02ce4b437312083f8d543da2727b1324e7feaa
David Kleiven <davidkleiven446@gmail.com>
2019-03-14 13:19:11 +0100

    Fix test

--- tests/test_landau_polynomial.py
+++ tests/test_landau_polynomial.py
@@ -24,8 +24,10 @@ class TestLandauPolynomial(unittest.TestCase):
             F1 = conc1**2
             conc2 = np.linspace(0.6, 1.0, 100)
             F2 = 0.5*(conc2-0.9)**2
+            F = np.concatenate((F1, F2))
+            conc = np.concatenate((conc1, conc2))
             poly = TwoPhaseLandauPolynomial(c1=0.0, c2=0.5)
-            poly.fit(conc1, F1, conc2, F2)
+            poly.fit(conc, F)
             # show_fit(poly, conc1, conc2, F1, F2)
         except Exception as exc:
             error = True
