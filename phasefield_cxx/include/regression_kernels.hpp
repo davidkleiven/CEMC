40dc41d7b24e79d05bd5b96357275b87e63a29fb
David Kleiven <davidkleiven446@gmail.com>
2019-03-15 15:41:30 +0100

    Add C++ regression kernel

---
+++ phasefield_cxx/include/regression_kernels.hpp
@@ -0,0 +1,33 @@
+#ifndef REGRESSION_KERNEL_H
+#define REGRESSION_KERNEL_H
+
+class RegressionKernel{
+    public:
+        RegressionKernel();
+
+        /** Evaluate the kernel */
+        virtual double evaluate(double x) const = 0;
+        
+        /** Calculate the derivative of the kernel */
+        virtual double deriv(double x) const = 0;
+};
+
+class QuadraticKernel: public RegressionKernel{
+    public:
+        QuadraticKernel(double width): RegressionKernel(), width(width){};
+
+        /** Evaluate the kernel */
+        virtual double evaluate(double x) const override final;
+
+        /** Calculate the derivative of the kernel */
+        virtual double deriv(double x) const override final;
+
+        /** Check if the point is outside the support of the kernel */
+        bool is_outside_support(double x) const;
+    private:
+        double width{1.0};
+
+        /** Calculate the amplitude of the kernel */
+        double amplitude() const{return 3.0*width/2.0};
+};
+#endif
---
+++ phasefield_cxx/src/regression_kernels.cpp
@@ -0,0 +1,21 @@
+#include "regression_kernels.hpp"
+
+double QuadraticKernel::evaluate(double x) const{
+    if (is_outside_support(x)){
+        return 0.0;
+    }
+
+    return amplitude()*(1.0 - x*x/(width*width));
+}
+
+bool QuadraticKernel::is_outside_support(double x) const{
+    return x < -width || x > width;
+}
+
+double QuadraticKernel::deriv(double x) const{
+    if (is_outside_support(x)){
+        return 0.0;
+    }
+
+    return -2*amplitude()*x/(width*width);
+}
