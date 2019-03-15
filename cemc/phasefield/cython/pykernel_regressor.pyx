ea6f6356b4db1f6d63e84bba416c47fc21dccd3b
David Kleiven <davidkleiven446@gmail.com>
2019-03-15 18:46:04 +0100

    Support array

--- cemc/phasefield/cython/pykernel_regressor.pyx
+++ cemc/phasefield/cython/pykernel_regressor.pyx
@@ -3,6 +3,7 @@
 from cemc.phasefield.cython.kernel_regressor cimport KernelRegressor
 from cython.operator cimport dereference
 from libcpp.vector cimport vector
+import numpy as np
 
 cdef class PyKernelRegressor:
     cdef KernelRegressor *thisptr
@@ -14,13 +15,23 @@ cdef class PyKernelRegressor:
         del self.thisptr
 
     def evaluate(self, x):
-        return self.thisptr.evaluate(x)
+        if np.isscalar(x):
+            return self.thisptr.evaluate(x)
+        return [self.thisptr.evaluate(x[i]) for i in range(len(x))]
 
     def deriv(self, x):
-        return self.thisptr.deriv(x)
+        if np.isscalar(x):
+            return self.thisptr.deriv(x)
+        return [self.thisptr.deriv(x[i]) for i in range(len(x))]
 
     def set_kernel(self, PyRegressionKernel kernel):
         self.thisptr.set_kernel(dereference(kernel.thisptr))
+
+    def evaluate_kernel(self, i, x):
+        if np.isscalar(x):
+            return self.thisptr.evaluate_kernel(i, x)    
+        
+        return [self.thisptr.evaluate_kernel(i, x[j]) for j in range(len(x))]
 
     def set_coeff(self, coeff):
         cdef vector[double] vec_coeff
