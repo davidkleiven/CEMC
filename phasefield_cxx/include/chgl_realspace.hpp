aa01f2362a75936f34b5a3ba2999a36ec2263904
David Kleiven <davidkleiven446@gmail.com>
2019-04-10 15:43:35 +0200

    Implemented build matrix

--- phasefield_cxx/include/chgl_realspace.hpp
+++ phasefield_cxx/include/chgl_realspace.hpp
@@ -1,6 +1,8 @@
 #ifndef CHGL_REALSPACE_H
 #define CHGL_REALSPACE_H
 #include "chgl.hpp"
+#include "sparse_matrix.hpp"
+#include <array>
 
 template<int dim>
 class CHGLRealSpace: public CHGL<dim>{
@@ -8,8 +10,14 @@ public:
     CHGLRealSpace(int L, const std::string &prefix, unsigned int num_gl_fields, \
          double M, double alpha, double dt, double gl_damping, 
          const interface_vec_t &interface);
+
+    /** Build the matrix for CHGL equations */
+    void build2D();
 private:
     unsigned int implicitDir{0};
+    std::array<SparseMatrix, dim+1> matrices;
+
+    unsigned int node_index(const MMSP::vector<int> &pos) const;
 };
 
 #endif
--- phasefield_cxx/src/chgl_realspace.cpp
+++ phasefield_cxx/src/chgl_realspace.cpp
@@ -5,3 +5,68 @@ CHGLRealSpace<dim>::CHGLRealSpace(int L, const std::string &prefix, unsigned int
          double M, double alpha, double dt, double gl_damping, 
          const interface_vec_t &interface): CHGL<dim>(L, prefix, num_gl_fields, M, alpha, dt, gl_damping, interface){};
 
+
+template<int dim>
+void CHGLRealSpace<dim>::build2D(){
+    if (dim != 2){
+        throw runtime_error("Build2D should never be called when dimension is not 2!");
+    }
+
+    MMSP::grid<2, grid> indexGrid(1, 0, L, 0, L);
+    for (unsigned int i=0;i<MMSP::nodes(indexGrid);i++){
+        indexGrid(i) = i;
+    }
+
+    // Build the matrix for the Cahn-Hilliard part
+    double factor = 2.0*alpha*M*dt;
+    SparseMatrix& matrix = matrices[0];
+    for (unsigned int i=0;i<MMSP::nodes(*this->grid_ptr);i++){
+        matrix.insert(i, i, 1.0 + 20*factor);
+
+        // Retrive node at position +- 1
+        for (unsigned int dir=0;dir<2;dir++)
+        for (int j=-1;j<2;j+=2){
+            MMSP::vector<int> pos = grid_ptr->position(i);
+            pos[dir] += j;
+            unsigned int col = indexGrid(pos);
+            matrix.insert(i, indx, -8*factor);
+        }
+
+        // Calculate factor at position +- 2
+        for (unsigned int dir=0;dir<2;dir++)
+        for (int j=-2;j<5;j+=4){
+            MMSP::vector<int> pos = grid_ptr->position(i);
+            pos[dir] += j;
+            unsigned int col = indexGrid(pos);
+            matrix.insert(i, indx, factor);
+        }
+
+        // Calculate the cross terms
+        for (int ix=-1;ix<2;ix+=2)
+        for (int iy=-1;iy<2;iy+=2){
+            MMSP::vector<int> pos = grid_ptr->position(i);
+            pos[0] += ix;
+            pos[1] += iy;
+            unsigned int col = indexGrid(pos);
+            matrix.insert(i, indx, 2*factor);
+        }
+    }
+
+    // Build GL part
+    factor = 2*L*dt;
+    for (unsigned int field=1;field<dim+1;field++){
+        SparseMatrix& mat = matrices[field];
+        for (unsigned int i=0;i<MMSP::nodes(*this->grid_ptr);i++){
+            matrix.insert(i, i, 1.0 + 2*factor*(interface[field-1][0] + interface[field-1][1]));
+
+            for (unsigned int dir=0;dir<2;dir++)
+            for (int ix=-1;ix<2;ix+=2){
+                MMSP::vector<int> pos = grid_ptr->position(i);
+                pos[dir] += ix;
+                unsigned int col = indexGrid(pos);
+                matrix.insert(i, col, -factor*interface[field-1][dir]);
+            }
+        }
+    }
+}
+
